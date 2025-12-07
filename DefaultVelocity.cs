f97c468064a69fb54a0113237f41ef7fd6e3a75b
Riryan <anthonyfmitchell@gmail.com>
2025-12-07 08:01:51 -0600

    Update DefaultVelocity.cs

--- DefaultVelocity.cs
+++ DefaultVelocity.cs
@@ -1,11 +1,17 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class DefaultVelocity : MonoBehaviour
 {
-    public Rigidbody rigidBody;
+    [Header("Initial Rigidbody velocity (world space)")]
     public Vector3 velocity;
+
+    Rigidbody rigidBody;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
 
     void Start()
     {
